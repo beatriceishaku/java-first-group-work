704324d185b818c94218da2b3be0e3a03806539e
Beatrice <beatriceishaku45@gmail.com>
2023-05-03 08:24:32 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("what formula do you want to solve?");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("kinetic energy");
+        System.out.println("potential energy");
         System.out.println("");
         System.out.println("");
         System.out.println("\n");
