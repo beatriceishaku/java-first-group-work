a3eb5577beafcb5c7c0b75f2feb33f30539c232b
Beatrice <beatriceishaku45@gmail.com>
2023-05-03 08:13:55 -0700

    Update Energy.java

--- Energy.java
+++ Energy.java
@@ -3,4 +3,7 @@ public class Energy extends Physics {
     double kineticEnergy(float mass, float velocity) {
         return (0.5 * mass) * (velocity * velocity);
     }
+    double potentialEnergy(int mass, int gravity, int height){
+        return(mass * gravity * height);
+    }
     }
