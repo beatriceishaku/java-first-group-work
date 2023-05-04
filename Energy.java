5c7e4b2a117e7f00f784cf156c0f13ef7ab99683
Beatrice <beatriceishaku45@gmail.com>
2023-05-04 16:02:26 +0100

    Update Energy.java

--- Energy.java
+++ Energy.java
@@ -1,12 +1,12 @@
 from operations.java import *;
 public class Energy extends Physics {
     double kineticEnergy(float mass, float velocity) {
-        return (0.5 * mass) * (velocity * velocity);
+        return multiply(multiply(0.5 ,mass),pow2(velocity));
     }
     double potentialEnergy(int mass, float gravity, int height){
-        return(mass * gravity * height);
+        return multiply(multiply(mass, gravity), height));
     }
     double velocity(float distance, float time){
-        return( distance/ time );
+        return divide(distance, time);
         }
     }
