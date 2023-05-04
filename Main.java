b348c29cb0bf60801beef13c920ac6b975db3032
AdoleJerry <131052563+AdoleJerry@users.noreply.github.com>
2023-05-04 16:13:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("what formula do you want to solve?");
-        System.out.println("kinetic energy");
-        System.out.println("potential energy");
-        System.out.println("velocity");
-        System.out.println("");
-        System.out.println("\n");
+        System.out.println("1 for kinetic energy");
+        System.out.println("2 for potential energy");
+        System.out.println("3 for velocity");
+        System.out.println("4 for ");
+        System.out.println("5 for ");
         Scanner input = new Scanner(System.in);
         int insert = input.nextInt();
+        switch (insert){
+            case 1:
+                System.out.println("Enter the mass");
+                mass = input.nextFloat();
+                System.out.println("Enter the velocity");
+                velocity = input.nextFloat();
+                return kineticEnergy;
+                
+            case 2:
+                
+                System.out.println("Enter the mass");
+                mass = input.nextFloat();
+                System.out.println("Enter the Height");
+                height = input.nextFloat();   
+                return potentialEnergy;
+                
+            case 3:
+                
+                System.out.println("Enter the distance");
+                distance = input.nextFloat();
+                System.out.println("Enter the time");
+                time = input.nextFloat();
+                return velocity;
+                                   
+                
+        
 
     }
 }
