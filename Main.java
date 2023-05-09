241e8449bf91129aa20c58434d6bb1fa05650e34
OnigbodeNathan <131052547+OnigbodeNathan@users.noreply.github.com>
2023-05-09 20:54:08 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 package UI;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Constructor called");
-        GeometryUI geometryUI = new GeometryUI();
-        geometryUI.mainUI();
+
+        PhysicsUI physicsUI = new PhysicsUI();
+        physicsUI.mainUI();
 
     }
 }]git push origin master
